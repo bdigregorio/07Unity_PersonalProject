04f552bda5c03fbf3af857f2c0209e9dcaeee925
Benjamin DiGregorio <ben@digregor.io>
2021-01-02 22:05:41 -0800

    Add script for camera to follow player object

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public GameObject player;
+    
+    private readonly Vector3 _cameraOffset = new Vector3(0, 3, -5);
+
+    private void Update()
+    {
+        transform.position = player.transform.position + _cameraOffset;
+    }
+}
