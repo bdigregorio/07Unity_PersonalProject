a8918a2a8407742b87bdd907526eab69bf97285e
Benjamin DiGregorio <ben@digregor.io>
2021-01-09 21:57:43 -0800

    Replace translation on player with translation on environment objects

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,18 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour{
-    private float engineSpeed = 10.0f;
 
-    void Start() {
+    private void Start() {
         
     }
 
     private void Update() {
-        applyEnginePower();
-    }
-
-    private void applyEnginePower() {
-        float z = engineSpeed * Time.deltaTime;
-        transform.Translate(0, 0, z);
     }
 }
