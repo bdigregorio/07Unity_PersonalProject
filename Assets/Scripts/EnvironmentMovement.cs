c046d977ba5169e0a801dad0a635bd0872dd9b75
Benjamin DiGregorio <ben@digregor.io>
2021-01-16 19:35:29 -0800

    Destroy out of bounds objects

--- Assets/Scripts/EnvironmentMovement.cs
+++ Assets/Scripts/EnvironmentMovement.cs
@@ -4,8 +4,20 @@ using UnityEngine;
 
 public class EnvironmentMovement : MonoBehaviour {
     public float engineSpeed = 10.0f;
+    private float zDestroyBounds = -5.0f;
 
     private void Update() {
+        SimulateEngineMovement();
+        RemoveOutOfBounds();
+    }
+
+    private void SimulateEngineMovement() {
         transform.Translate(Vector3.back * engineSpeed * Time.deltaTime);
+    }
+
+    private void RemoveOutOfBounds() {
+        if (transform.position.z < zDestroyBounds) {
+            Destroy(gameObject);
+        }
     }
 }
